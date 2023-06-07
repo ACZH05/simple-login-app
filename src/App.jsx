a0ba2cdf7cb7c2b698599e1d4273891587fb7b44
Alfred Chin <alfredchin2005@gmail.com>
2023-06-07 17:24:46 +0800

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import useLocalStorage from 'use-local-storage';
 import Home from "./pages/Home"
@@ -7,14 +8,32 @@ import Dashboard from './pages/Dashboard';
 import { AuthContext } from './AuthContext';
 import RequireAuth from './components/requireAuth';
 
+function NavBar() {
+  return (
+    <Navbar bg="dark" variant='dark' expand="lg">
+      <Container>
+        <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Nav className="me-auto">
+            <Nav.Link href="/login">Login Page</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/profile">Profile</Nav.Link>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+          </Nav>
+      <Button variant='outline-light'>Logout</Button>
+      </Container>
+    </Navbar>
+  )
+}
 
 function App() {
   const [token, setToken] = useLocalStorage("token", null)
   return (
     <AuthContext.Provider value={{token, setToken}}>
+      <NavBar />
 `      <BrowserRouter>
         <Routes>
-          <Route element={ <Home /> } path='/' />
+          <Route element={ <Home /> } path='/home' />
           <Route element={ <Login /> } path='/login' />
           <Route element={
             <RequireAuth><Dashboard /></RequireAuth>
