3da8ffc9cd245f5d90c36df710d98455eebefe77
Alfred Chin <alfredchin2005@gmail.com>
2023-06-07 09:47:03 +0800

    Import router and home into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={ <Home /> } path='/' />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
