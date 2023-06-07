5316e7a76b076b4a61f8dcac92cefa7c74e45356
Alfred Chin <alfredchin2005@gmail.com>
2023-06-07 12:01:13 +0800

    Add token into local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { useState } from 'react';
+import useLocalStorage from 'use-local-storage';
 import Home from "./pages/Home"
 import Login from "./pages/Login.jsx"
 import Dashboard from './pages/Dashboard';
 import { AuthContext } from './AuthContext';
+import RequireAuth from './components/requireAuth';
 
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useLocalStorage("token", null)
   return (
     <AuthContext.Provider value={{token, setToken}}>
 `      <BrowserRouter>
         <Routes>
           <Route element={ <Home /> } path='/' />
           <Route element={ <Login /> } path='/login' />
-          <Route element={ <Dashboard /> } path='/dashboard' />
+          <Route element={
+            <RequireAuth><Dashboard /></RequireAuth>
+          }
+          path='/dashboard'
+          />
         </Routes>
       </BrowserRouter>`
     </AuthContext.Provider>
